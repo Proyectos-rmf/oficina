e2cfffa26dc8c62c6de532d4276bb81b7bd8cc3a
Proyectos <ricardomascareo@hotmail.com>
2020-06-10 23:11:38 -0500

    casa
    login

--- src/app/core/admin/login/login.component.ts
+++ src/app/core/admin/login/login.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: []
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 

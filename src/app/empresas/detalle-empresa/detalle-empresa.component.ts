711a8e195c79ae8a0c4ea10506f6be9d5ab600b5
Proyectos <ricardomascareo@hotmail.com>
2020-08-14 19:21:17 -0500

    otro
    otro

--- src/app/empresas/detalle-empresa/detalle-empresa.component.ts
+++ src/app/empresas/detalle-empresa/detalle-empresa.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 import { UtilService } from './../../services/util.service';
 import { CrudService } from '../empresas.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-detalle-empresa',
@@ -54,9 +55,83 @@ export class DetalleEmpresaComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log('Form->' + JSON.stringify(this.empresaForm.value));
-    console.log(this.empresaForm.value);
-    this.crudApi.creaEmpresa(this.empresaForm.value, 'empresa');
+    const ipAPI = '//api.ipify.org?format=json';
+
+    swal.queue([{
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      title: 'aaaaa',
+      showConfirmButton: false,
+      showCloseButton: false,
+      showCancelButton: false,
+      onOpen: () => {
+          swal.showLoading();
+          return fetch(ipAPI, {
+              method: 'POST',
+              body: 'viendo',
+              headers: {
+                  'Accept': 'application/json, text/plain, */*',
+                  'Content-Type': 'application/x-www-form-urlencoded',
+              }
+          })
+                  .then(response => response.json())
+                  .then(data => {
+                      swal.hideLoading();
+                      if (data.status === 'success') {
+                          swal.update({
+                              allowEscapeKey: false,
+                              allowOutsideClick: false,
+                              showConfirmButton: false,
+                              showCloseButton: false,
+                              showCancelButton: false,
+                              type: 'success',
+                              title: false,
+                              html: data.html
+                          });
+                      } else {
+                          swal.update({
+                              icon: 'error',
+                              title: false,
+                              html: data.html,
+                              allowEscapeKey: true,
+                              allowOutsideClick: true,
+                              showConfirmButton: true,
+                          });
+                      }
+                  })
+                  .catch(() => {
+                      swal.hideLoading();
+                      swal.update({
+                          icon: 'error',
+                          title: 'Save request error!',
+                          html: false
+                      });
+                  });
+      }
+  }]);
+
+
+/*
+    swal.fire({
+
+      title: 'Guardando Información ...',
+      showConfirmButton: false,
+    });
+*/
+
+    this.crudApi.creaEmpresa(this.empresaForm.value, 'empresa')
+      .then((res) => {
+        /* swal.fire({
+          icon: 'success',
+          title: 'Empresa Creada',
+          showConfirmButton: false,
+          allowOutsideClick: false,
+          timer: 2000
+        }); */
+      })
+      .catch(err =>  {
+        console.log('err', err.message);
+      });
   }
 
   isError(campo: string, tipo: string): string {
